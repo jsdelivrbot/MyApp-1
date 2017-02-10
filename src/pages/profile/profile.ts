8183ba401dcd6f5a89b2c495ca54a3a43fb372f5
DEV <dev@Blake-Zeislers-computer.local>
2017-02-09 22:14:10 -0500

    Profile pic from firebase and profile pic actionsheet

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { Platform, ActionSheetController } from 'ionic-angular';
+import { Camera } from 'ionic-native';
 
 import { ProfileData } from '../../providers/profile-data';
 import { AuthService } from '../../providers/auth-service';
@@ -23,11 +25,13 @@ export class ProfilePage {
   public relationship: any;
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public profileData: ProfileData, public alertCtrl: AlertController) {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public profileData: ProfileData, public alertCtrl: AlertController, public actionSheetCtrl: ActionSheetController, public platform: Platform) {
   this.profileData = profileData;
   this.profileData.getUserProfile().on('value', (data) => {
       this.userProfile = data.val();
     });
+
   }
 
   ionViewDidLoad() {
@@ -160,6 +164,38 @@ updateRelationship(){
   });
   alert.present();
 }
+
+profilePicActionSheet() {
+    let profilePicSheet = this.actionSheetCtrl.create({
+      title: 'Upload your Profile Picture',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Take a Picture',
+          role: 'destructive',
+          icon: !this.platform.is('ios') ? 'camera' : null,
+          handler: () => {
+            console.log('Take a Picture clicked');
+          }
+        },{
+          text: 'Select from Gallery',
+          icon: !this.platform.is('ios') ? 'images' : null,
+          handler: () => {
+            console.log('Select from Gallery clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          icon: !this.platform.is('ios') ? 'close' : null,
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    profilePicSheet.present();
+  }
+
 
 
 }
