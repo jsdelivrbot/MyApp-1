a42fbc4a3d972ff461aa45e724ea55ecb5650653
DEV <dev@Blake-Zeislers-computer.local>
2017-02-27 00:15:10 -0500

    Updated Chat Page with filter for Groups and People

--- src/pages/chats/chats.ts
+++ src/pages/chats/chats.ts
@@ -4,6 +4,10 @@ import firebase from 'firebase';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
 import { LoadingProvider } from '../../providers/loading';
 import { GroupAddPage } from '../group-add/group-add';
+import { FormControl } from '@angular/forms';
+
+
+
 
 /*
   Generated class for the Chats page.
@@ -16,14 +20,48 @@ import { GroupAddPage } from '../group-add/group-add';
   templateUrl: 'chats.html'
 })
 export class ChatsPage {
-	groups: FirebaseListObservable<any>;
-	people: FirebaseListObservable<any>;
-	userProfile: any;
+	groupsRef: any;
+	groupList: any;
+	filteredGroupList: any;
+	peopleRef: any;
+	peopleList: any;
+	filteredPeopleList: any;
 	chatType: string = "groups";
+	
 
   constructor(public navCtrl: NavController, public navParams: NavParams, af: AngularFire) {
-  this.groups = af.database.list('/Weddings/0/weddingGroups');
-  this.people = af.database.list('/Weddings/0/weddingPeople');
+  	this.groupsRef = firebase.database().ref('/Weddings/0/weddingGroups');
+  	this.groupsRef.on('value', groupList => {
+  	let groups = [];
+  		groupList.forEach( group => {
+    		groups.push(group.val());
+  		});
+  		this.groupList = groups;
+  		this.filteredGroupList = groups;
+  	});
+
+  	this.initializeGroups();
+  	
+  	this.peopleRef = firebase.database().ref('/Weddings/0/weddingPeople');
+  	this.peopleRef.on('value', peopleList => {
+  	let people = [];
+  		peopleList.forEach( person => {
+    		people.push(person.val());
+  		});
+  		this.peopleList = people;
+  		this.filteredPeopleList = people;
+  	});
+
+  	this.initializePeople();
+
+  }
+
+  initializeGroups(): void {
+  	this.groupList = this.filteredGroupList;
+  }
+
+  initializePeople(): void {
+  	this.peopleList = this.filteredPeopleList;
   }
 
   ionViewDidLoad() {
@@ -33,6 +71,55 @@ export class ChatsPage {
   goToGroupAddPage(){
   this.navCtrl.push(GroupAddPage);
 
+  }
+
+  searchGroups(searchGroupBar) {
+  	// Reset items back to all of the items
+  	this.initializeGroups();
+
+  	// set searchedGroup to the value of the searchbar
+  	var searchedGroup = searchGroupBar.srcElement.value;
+
+  	// if the value is an empty string don't filter the items
+  	if (!searchedGroup) {
+    	return;
+  	}
+  	this.groupList = this.groupList.filter((group) => {
+    	if(group.groupName && searchedGroup) {
+      	if (group.groupName.toLowerCase().indexOf(searchedGroup.toLowerCase()) > -1) {
+        	return true;
+      	}
+      	return false;
+    	}
+  	});
+  }
+
+  searchPeople(searchPeopleBar) {
+  	// Reset items back to all of the items
+  	this.initializePeople();
+
+
+  	// set searchedGroup to the value of the searchbar
+  	var searchedPeople = searchPeopleBar.srcElement.value;
+
+  	// if the value is an empty string don't filter the items
+  	if (!searchedPeople) {
+    	return;
+  	}
+  	this.peopleList = this.peopleList.filter((person) => {
+    	if(person.fullname && searchedPeople) {
+      		if (person.fullname.toLowerCase().indexOf(searchedPeople.toLowerCase()) > -1) {
+        		return true;
+      		}
+      		if (person.firstname.toLowerCase().indexOf(searchedPeople.toLowerCase()) > -1) {
+        		return true;
+      		}
+      		if (person.lastname.toLowerCase().indexOf(searchedPeople.toLowerCase()) > -1) {
+        		return true;
+      		}
+      		return false;
+    	}
+  	});
   }
 
 }
