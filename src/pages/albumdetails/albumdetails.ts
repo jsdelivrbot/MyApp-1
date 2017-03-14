730a942310f5b3ae8bdfeddcacca0ea1ef66dcca
DEV <dev@Blake-Zeislers-computer.local>
2017-03-13 23:51:16 -0400

    Updated AlbumdetailsPage with ImagePicker plugin and write to Firebase (single picture only)

--- src/pages/albumdetails/albumdetails.ts
+++ src/pages/albumdetails/albumdetails.ts
@@ -2,9 +2,9 @@ import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams, Content } from 'ionic-angular';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
 import firebase from 'firebase';
-import { Subject } from 'rxjs/Subject';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { ImagePicker } from 'ionic-native';
+
+declare var window: any;
 
 /*
   Generated class for the Albumdetails page.
@@ -19,42 +19,22 @@ import { ImagePicker } from 'ionic-native';
 export class AlbumdetailsPage {
   @ViewChild(Content) content: Content;
 	album;
-	photos: FirebaseListObservable<any[]>;
-	photoLimit: Subject<any>;
-	public limit = 0;
-  photoCountRef: any;
   photoList: any;
-  photoCount: any;
-  rows: any;
+  photoCountRef: any;
+	public limit = 0;
   public newLimit = 10;
   public needMorePhotos = false;
+  public photoURL: any;
+  public photoRef: any;
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams, af: AngularFire) {
   	this.album = navParams.data.album;
-    this.photoLimit = new Subject();
     this.initializePhotos(this.newLimit);
-    /*this.photoCountRef = firebase.database().ref('/Weddings/0/weddingAlbums/' + this.album.$key + '/albumPhotos').limitToLast(this.newLimit);
-    this.photoCountRef.on('value', photoList => {
-    let photos = [];
-      photoList.forEach( photo => {
-        photos.push(photo.val());
-      });
-      this.photoList = photos;
-    });*/
-    //console.log(this.photoList.length);
-    //this.rows = (this.photoList.length / 2);
-
-  	/*this.photos = af.database.list('/Weddings/0/weddingAlbums/' + this.album.$key + '/albumPhotos', {
-  		query: {
-  			limitToLast: this.photoLimit
-  		}
-  	});*/
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AlbumdetailsPage');
-    //this.loadMorePhotos(8);
     
   }
 
@@ -72,33 +52,91 @@ export class AlbumdetailsPage {
 
   }
 
-  doInfinite(infiniteScroll) {
-    console.log('Begin async operation');
-
-    setTimeout(() => {
-      for (let i = 0; i < 300; i++) {
-        this.photoList.push( this.newLimit );
-      }
-
-      console.log('Async operation has ended');
-      this.newLimit = this.newLimit + 1;
-      this.initializePhotos(this.newLimit);
-      infiniteScroll.complete();
-    }, 500);
-  }
 
   loadMorePhotos(limit){
-    //this.limit = this.limit + limit;
     this.needMorePhotos = false;
     if(this.newLimit == this.photoList.length){
     this.newLimit = this.newLimit + limit;
-    console.log(this.newLimit + ' ' + this.photoList.length);
     this.initializePhotos(this.newLimit);
-  	//this.photoLimit.next(this.limit);
     this.scrollBottom();
     }
     return this.needMorePhotos;
   }
+
+  private openGallery(){
+    let options = {
+    maximumImagesCount: 10,
+    quality: 100
+    }
+
+    ImagePicker.getPictures(options).then((file_uris) => {
+      // convert picture to blob
+      return this.makeFileIntoBlob(file_uris);
+    }).then((imageBlob) => {
+      // upload the blob
+      return this.uploadToFirebase(imageBlob);
+    }).then((uploadSnapshot: any) => {
+      // store reference to storage in database
+      return this.saveToPhotoAlbum(uploadSnapshot);
+    }).then((uploadSnapshot: any) => {
+    }, (_error) => {
+    alert('Error ' + (_error.message || _error));
+    });
+
+  }
+
+  makeFileIntoBlob(file_uris) {
+
+  return new Promise((resolve, reject) => {
+    window.resolveLocalFileSystemURL(file_uris[0], (fileEntry) => {
+
+      fileEntry.file((resFile) => {
+
+        var reader = new FileReader();
+        reader.onloadend = (evt: any) => {
+          var imgBlob: any = new Blob([evt.target.result], { type: 'image/jpeg' });
+          imgBlob.name = 'sample.jpg';
+          resolve(imgBlob);
+        };
+
+        reader.onerror = (e) => {
+          console.log('Failed file read: ' + e.toString());
+          reject(e);
+        };
+
+        reader.readAsArrayBuffer(resFile);
+      });
+    });
+  });
+}
+
+uploadToFirebase(imageBlob) {
+  var fileName = 'TEST' + '.jpg';
+
+  return new Promise((resolve, reject) => {
+    var fileRef = firebase.storage().ref('/Weddings/0/weddingAlbums/' + this.album.$key + '/' + fileName);
+
+    var uploadTask = fileRef.put(imageBlob);
+
+    uploadTask.on('state_changed', (snapshot) => {
+      console.log('snapshot progess ' + snapshot);
+    }, (error) => {
+      reject(error);
+    }, () => {
+      // completion...
+      resolve(uploadTask.snapshot);
+    });
+  });
+}
+
+saveToPhotoAlbum(uploadSnapshot) {
+  this.photoURL = uploadSnapshot.downloadURL;
+  this.photoRef = firebase.database().ref('/Weddings/0/weddingAlbums/' + this.album.$key + '/albumPhotos/');
+  this.photoRef.push({
+      photoURL: this.photoURL,
+    });
+
+}
 
 
   // Scroll to bottom of page after a short delay.
