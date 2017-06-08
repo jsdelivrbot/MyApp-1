bc72cddd6b1b65743e2d1357ad499405554d5648
DEV <dev@Blake-Zeislers-computer.local>
2017-06-07 23:49:50 -0400

    Updated WeddingJoin Page to compare firebase db for wedding key

--- src/pages/wedding-join/wedding-join.ts
+++ src/pages/wedding-join/wedding-join.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, AlertController } from 'ionic-angular';
 import { FormBuilder, Validators } from '@angular/forms';
+import firebase from 'firebase';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
+
+import { TabsPage } from '../tabs/tabs';
 
 @Component({
   selector: 'page-wedding-join',
@@ -9,17 +13,54 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class WeddingJoinPage {
 	public weddingJoinForm;
 	submitAttempt: boolean = false;
+	private weddingKey: String;
+	weddingKeyList: FirebaseListObservable<any>;
+	weddingKeyString: String;
+	private weddingKeyArray = [];
+	keyMatch: boolean = false;
 
-  constructor(public navCtrl: NavController, public formBuilder: FormBuilder, public alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder, public alertCtrl: AlertController, public af: AngularFire) {
   	this.weddingJoinForm = formBuilder.group({
       weddingKey: ['', Validators.compose([Validators.required])]
     });
+	this.keyMatch = false;
   }
 
-  joinWedding(){
+  joinWedding(weddingKey){
   	this.submitAttempt = true;
-  	//TODO: read from firebase the wedding keys, then if doesnt match alert the user their key does not match. Else, goto TabsPage for matching wedding info. 
-  	console.log("TODO: compare weddingKey to firebase weddingKeyList then if match goto TabsPage for Wedding, else alert that no wedding with that key exists")
+  	this.weddingKey = weddingKey;
+  	
+  	if(this.weddingKey != null){
+  	this.weddingKeyList = this.af.database.list('/Weddings', {
+  		query: {
+  			orderByKey: true,
+  			equalTo: this.weddingKey
+  		}
+  	});
+  		this.weddingKeyList.subscribe(keys => {
+    		// items is an array
+    		keys.forEach(key => {
+        		this.weddingKeyArray.push(key.$key);
+    			});
+    		if(this.weddingKeyArray[0] == this.weddingKey){
+    			this.weddingKeyArray = [];
+    			this.navCtrl.setRoot(TabsPage);
+            	this.navCtrl.push(TabsPage);
+            	//TODO: Push to firebase to update firstLogin variable to false
+    		}
+    		else{
+    		let alert = this.alertCtrl.create({
+          		title: 'Wedding Key not found!',
+          		subTitle: 'Ask your wedding creator for a valid wedding key',
+          		buttons: ['OK']
+          		});
+            alert.present();
+
+    		}
+
+		});
+  	}
+  	
   	
   }
 
